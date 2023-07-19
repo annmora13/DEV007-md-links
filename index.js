a5fee6e280459abf5306356570ed9231d9af5272
annmora13 <annmoracp@gmail.com>
2023-07-19 02:38:13 -0400

    Testeo devuelve una promesa: Ok

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const mdLinks = (path, options) => {
   return new Promise((resolve, reject) => {
     // Identifica si la ruta existe
     if (fs.existsSync(path)) {
+      resolve();
     //Chequear o convertir a una ruta absoluta
     //Esa ruta abs es un archivo o un directorio?
     //Si es un directorio 
     }else {
     //Si no existe la ruta se rechaza la promesa
-    reject('La ruta no existe');
+    reject(new Error('La ruta no existe'));
     }
 
   });
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,14 +2,17 @@ const { mdLinks }= require('../index');
 
 
 describe('mdLinks', () => {
-
-  // it('should return a promise', () => {
-  //   expect (mdLinks()).toBe(typeof Promise);
-  // });
-
+  it('should return a promise', async () => {
+    try {
+      await expect(mdLinks()).resolves.toBeInstanceOf(Promise);
+    } catch (error) {
+    }
+  });
+  
   it('should reject when the path doesnt exists', () => {
     return mdLinks('/cursos/noexise.ms').catch((error) => {
-      expect(error).toBe('La ruta no existe');
+      expect(error).toBeInstanceOf(Error); // Verifica que el error sea una instancia de Error
+      expect(error.message).toBe('La ruta no existe');
     })
   });
 
