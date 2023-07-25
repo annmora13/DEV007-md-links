d7cd062fc14babfbc3253bb510be051ef5b1575b
annmora13 <annmoracp@gmail.com>
2023-07-25 15:41:32 -0400

    Act de análisis de si es archivo md o no

--- cli.js
+++ cli.js
@@ -1,6 +1,14 @@
-//Cli va sacar los argumentos con process 
-const { mdLinks } = require('./index.js');
-mdLinks('/noexiste/').then(() => {})
+//Cli va a ser la interacción entre la app y el usuario
+//Como interfaz en terminal
+// validate y stats
+const { mdLinks, getLinks }  = require('./index.js');
+const chalk = require('chalk');
+
+mdLinks('./Testing/testing.md').then((res) => {
+    console.log(chalk.blue(res));
+})
 .catch((error) => {
     console.log(error);
+    console.log(chalk.red('La ruta no existe'));
 });
+
--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const axios = require("axios");
+const { get } = require("https");
+
+// console.log(chalk.blue('Hello world!'));
+//console.log(axios.isCancel('something'));
 
 const mdLinks = (filePath, options) => {
   return new Promise((resolve, reject) => {
-    // Identifica si la ruta existe
+    let userPath;
+
     if (fs.existsSync(filePath)) {
-      resolve();
-    //Chequear si una ruta es absoluta
-    if (path.isAbsolute(filePath)){
-      resolve();
+      //Pregunta si la ruta es absoluta. Si sí la deja normal
+      // y si no la convierte a absoluta.
+      if (path.isAbsolute(filePath)) {
+        userPath = filePath;
+      } else {
+        userPath = path.resolve(filePath);
+      }
+      console.log(userPath);
+      //Evalúa la extensión
+      if (path.extname(userPath) === ".md") {
+        resolve("el archivo sí es md");
+        getLinks(filePath);
+      } else {
+        reject('el archivo no es md')
+      }
     } else {
-      // Ruta relativa
-      const absolutePath = path.resolve(filePath); // Convierte la ruta relativa a absoluta
-      resolve(absolutePath);
-    }
-    //Esa ruta abs es un archivo o un directorio?
-    //Si es un directorio 
-    } else {
-    //Si no existe la ruta se rechaza la promesa
-    reject(new Error('La ruta no existe'));
+      reject(new Error("La ruta no existe"));
     }
   });
 };
+const getLinks = (filePath) => {
+  console.log("aaa", filePath);
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf-8", (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        const regex = /\[(.*?)\]\((?!#)(.*?)\)/g;
+        const links = data.match(regex);
+        resolve(links);
+      }
+    });
+  });
+};
+
 //resolve y reject son callbacks -los que pasamos con then y cacth
 module.exports = {
-  mdLinks
+  mdLinks,
+  getLinks,
 };
